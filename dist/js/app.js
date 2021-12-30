a90d059356e6a0510e1bee3e6ed3fec0cac4a67d
Davit Petrosyan <davannsh@gmail.com>
2021-12-30 17:25:30 -0500

    Nav and Scroll

---
+++ dist/js/app.js
@@ -0,0 +1,15 @@
+// DOM settings
+
+var scrollButton = document.getElementById("up-arrow");
+
+window.onscroll = function() {scrollFunction()};
+
+// On-Scroll function that shows/hides the up scroll button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.opacity = 1;
+    }
+    else {
+        scrollButton.style.opacity = 0;
+    }
+}
